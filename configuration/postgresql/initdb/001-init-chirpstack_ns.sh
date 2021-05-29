8e6dfa75f1a209716de34245709f0bd7a1b0cd5d
Álvaro Zornoza <alvaro.zornoza@gmail.com>
2021-05-29 19:06:10 +0200

    Adding two network servers, eu and au

--- configuration/postgresql/initdb/001-init-chirpstack_ns.sh
+++ configuration/postgresql/initdb/001-init-chirpstack_ns.sh
@@ -2,6 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role chirpstack_ns with login password 'chirpstack_ns';
-    create database chirpstack_ns with owner chirpstack_ns;
+    create role chirpstack_ns_eu with login password 'chirpstack_ns_eu';
+    create database chirpstack_ns_eu with owner chirpstack_ns_eu;
+    create role chirpstack_ns_au with login password 'chirpstack_ns_au';
+    create database chirpstack_ns_au with owner chirpstack_ns_au;
 EOSQL
